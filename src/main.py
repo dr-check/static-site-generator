6281029de81411970ee4544eec5dc2fb0f79185a
dr-check <dustin.r.check@gmail.com>
2025-05-18 23:36:27 -0400

    E: Basic pages ready.

--- src/main.py
+++ src/main.py
@@ -1,262 +1,37 @@
 import re
+import os
+import shutil
+import sys
 from textnode import *
 from htmlnode import *
 from blocktype import *
+from textmorph import *
+from copystatic import *
+from gencontent import *
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
-    new_node = TextNode("Hello World", TextType.TEXT, "http://example.com")
-    print(new_node)
-    return new_node
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(tag=None, value=text_node.text)
-    elif text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text)
-    elif text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-    elif text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
-    elif text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, props={"href": text_node.url})
-    elif text_node.text_type == TextType.IMAGE:
-        return LeafNode("img", value=text_node.text, props={"src": text_node.url, "alt": text_node.text})
-    else:
-        raise ValueError(f"Unknown text type: {text_node.text_type}")
-    
-def text_to_textnodes(text):
-    nodes = [TextNode(text, TextType.TEXT)]
-    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
-    
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        split_nodes = []
-        sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0:
-            raise ValueError("invalid markdown, formatted section not closed")
-        for i in range(len(sections)):
-            if sections[i] == "":
-                continue
-            if i % 2 == 0:
-                split_nodes.append(TextNode(sections[i], TextType.TEXT))
-            else:
-                split_nodes.append(TextNode(sections[i], text_type))
-        new_nodes.extend(split_nodes)
-    return new_nodes
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-def extract_markdown_images(text):
-    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    matches = re.findall(pattern, text)
-    return matches
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
-def extract_markdown_links(text):
-    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    matches = re.findall(pattern, text)
-    return matches
-
-def split_nodes_image(old_nodes):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        original_text = old_node.text
-        images = extract_markdown_images(original_text)
-        if len(images) == 0:
-            new_nodes.append(old_node)
-            continue
-        for image in images:
-            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
-            if len(sections) != 2:
-                raise ValueError("invalid markdown, image section not closed")
-            if sections[0] != "":
-                new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            new_nodes.append(
-                TextNode(
-                    image[0],
-                    TextType.IMAGE,
-                    image[1],
-                )
-            )
-            original_text = sections[1]
-        if original_text != "":
-            new_nodes.append(TextNode(original_text, TextType.TEXT))
-    return new_nodes
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
-def split_nodes_link(old_nodes):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        original_text = old_node.text
-        links = extract_markdown_links(original_text)
-        if len(links) == 0:
-            new_nodes.append(old_node)
-            continue
-        for link in links:
-            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
-            if len(sections) != 2:
-                raise ValueError("invalid markdown, link section not closed")
-            if sections[0] != "":
-                new_nodes.append(TextNode(sections[0], TextType.TEXT))
-            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
-            original_text = sections[1]
-        if original_text != "":
-            new_nodes.append(TextNode(original_text, TextType.TEXT))
-    return new_nodes
-
-def markdown_to_blocks(markdown):
-    blocks = markdown.split("\n\n")
-    filtered_blocks = []
-    for block in blocks:
-        if block == "":
-            continue
-        block = block.strip()
-        filtered_blocks.append(block)
-    return filtered_blocks
-
-def block_to_block_type(block):
-    lines = block.split("\n")
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
-        return BlockType.HEADING
-    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
-        return BlockType.CODE
-    if block.startswith("> "):
-        for line in lines:
-            if not line.startswith("> "):
-                return BlockType.PARAGRAPH
-        return BlockType.QUOTE
-    if block.startswith("- "):
-        for line in lines:
-            if not line.startswith("- "):
-                return BlockType.PARAGRAPH
-        return BlockType.ULIST
-    if block.startswith("1. "):
-        i = 1
-        for line in lines:
-            if not line.startswith(f"{i}. "):
-                return BlockType.PARAGRAPH
-            i += 1
-        return BlockType.OLIST
-    return BlockType.PARAGRAPH
-
-
-
-def markdown_to_html_node(markdown):
-    blocks = markdown_to_blocks(markdown)
-    children = []
-    for block in blocks:
-        html_node = block_to_html_node(block)
-        children.append(html_node)
-    return ParentNode("div", children, None)
-
-
-def block_to_html_node(block):
-    block_type = block_to_block_type(block)
-    if block_type == BlockType.PARAGRAPH:
-        return paragraph_to_html_node(block)
-    if block_type == BlockType.HEADING:
-        return heading_to_html_node(block)
-    if block_type == BlockType.CODE:
-        return code_to_html_node(block)
-    if block_type == BlockType.OLIST:
-        return olist_to_html_node(block)
-    if block_type == BlockType.ULIST:
-        return ulist_to_html_node(block)
-    if block_type == BlockType.QUOTE:
-        return quote_to_html_node(block)
-    raise ValueError("invalid block type")
-
-
-def text_to_children(text):
-    text_nodes = text_to_textnodes(text)
-    children = []
-    for text_node in text_nodes:
-        html_node = text_node_to_html_node(text_node)
-        children.append(html_node)
-    return children
-
-
-def paragraph_to_html_node(block):
-    lines = block.split("\n")
-    paragraph = " ".join(lines)
-    children = text_to_children(paragraph)
-    return ParentNode("p", children)
-
-
-def heading_to_html_node(block):
-    level = 0
-    for char in block:
-        if char == "#":
-            level += 1
-        else:
-            break
-    if level + 1 >= len(block):
-        raise ValueError(f"invalid heading level: {level}")
-    text = block[level + 1 :]
-    children = text_to_children(text)
-    return ParentNode(f"h{level}", children)
-
-
-def code_to_html_node(block):
-    if not block.startswith("```") or not block.endswith("```"):
-        raise ValueError("invalid code block")
-    text = block[4:-3]
-    raw_text_node = TextNode(text, TextType.TEXT)
-    child = text_node_to_html_node(raw_text_node)
-    code = ParentNode("code", [child])
-    return ParentNode("pre", [code])
-
-
-def olist_to_html_node(block):
-    items = block.split("\n")
-    html_items = []
-    for item in items:
-        text = item[3:]
-        children = text_to_children(text)
-        html_items.append(ParentNode("li", children))
-    return ParentNode("ol", html_items)
-
-
-def ulist_to_html_node(block):
-    items = block.split("\n")
-    html_items = []
-    for item in items:
-        text = item[2:]
-        children = text_to_children(text)
-        html_items.append(ParentNode("li", children))
-    return ParentNode("ul", html_items)
-
-
-def quote_to_html_node(block):
-    lines = block.split("\n")
-    new_lines = []
-    for line in lines:
-        if not line.startswith(">"):
-            raise ValueError("invalid quote block")
-        new_lines.append(line.lstrip(">").strip())
-    content = " ".join(new_lines)
-    children = text_to_children(content)
-    return ParentNode("blockquote", children)
-        
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+main()
